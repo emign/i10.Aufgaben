d0947484e08b132f25f3a2dbcdb6dca3ae385dfe
nemig <nicolai.emig@amseegym.de>
2021-01-21 21:24:57 +0100

    Planetensystem fertig

--- Lektionen/02.GameLoop/src/commonMain/kotlin/main.kt
+++ Lektionen/02.GameLoop/src/commonMain/kotlin/main.kt
@@ -8,7 +8,39 @@ import com.soywiz.korio.file.std.*
 import com.soywiz.korma.geom.degrees
 import com.soywiz.korma.interpolation.Easing
 
-suspend fun main() = Korge(width = 800, height = 800, bgcolor = Colors["#2b2b2b"]) {
+suspend fun main() = Korge(width = 800, height = 800, bgcolor = Colors.DARKGRAY) {
+    val kreis = Kreis()
+
+    kreis.radius = 100.0
+    kreis.fill = Colors.GREENYELLOW
+
+    val breiteStage = this.stage.width
+    val hoeheStage = this.stage.height
+
+    kreis.x = breiteStage/2 - kreis.radius
+    kreis.y = hoeheStage/2 - kreis.radius
+
+    val farbArray = arrayOf(Colors.GREEN, Colors.DEEPPINK, Colors.AZURE, Colors.BISQUE,
+                            Colors.BLUE, Colors.RED, Colors.FUCHSIA, Colors.MEDIUMSPRINGGREEN)
+
+
+    kreis.addUpdater {
+        fill = farbArray.random()
+
+        if (y == hoeheStage - radius*2 || y == 0.0){
+            deltaY = deltaY * -1
+        }
+
+        if (x == breiteStage - radius*2 || x == 0.0){
+            deltaX = deltaX * -1
+        }
+
+        x = x + deltaX
+        y = y + deltaY
+
+    }
+
+    addChild(kreis)
 
 }
 
---
+++ Lektionen/03.Planetensystem/src/commonMain/kotlin/main.kt
@@ -0,0 +1,8 @@
+import com.soywiz.korge.Korge
+import com.soywiz.korge.view.addUpdater
+import com.soywiz.korim.color.Colors
+
+suspend fun main() = Korge(width = 1600, height = 1000, bgcolor = Colors.BLACK) {
+
+}
+
