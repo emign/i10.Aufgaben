fb7c1db854c2787145072a1e9ec9ba768f2c58ec
Nicolai Emig <nicolai.emig@amseegym.de>
2021-02-15 09:38:02 +0100

    Planetenlösung wieder hinzugefügt

---
+++ Lektionen/03.Planetensystem/src/commonMain/kotlin/Gasplanet.kt
@@ -0,0 +1,19 @@
+import com.soywiz.korim.color.RGBA
+
+class Gasplanet(
+        groesse : Double,
+        bahnradius : Double,
+        farbe : RGBA,
+        farbeHuelle : RGBA,
+        dickeHuelle : Double,
+        drehtSichUm : Stern,
+        mitGeschwindigkeit : Double
+) : Planet(
+        planetenRadius = groesse,
+        bahnradius = bahnradius,
+        farbe = farbe,
+        farbeHuelle = farbeHuelle,
+        dickeHuelle = dickeHuelle,
+        drehtSichUm = drehtSichUm,
+        mitGeschwindigkeit = mitGeschwindigkeit
+)
---
+++ Lektionen/03.Planetensystem/src/commonMain/kotlin/Stern.kt
@@ -0,0 +1,4 @@
+import com.soywiz.korge.view.Circle
+import com.soywiz.korim.color.Colors
+
+class Stern(groesse : Double = 50.0) : Circle(radius = groesse, fill = Colors.YELLOW)
