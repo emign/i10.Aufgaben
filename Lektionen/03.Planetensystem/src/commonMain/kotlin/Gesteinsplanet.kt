d4104fc9375a1fa3aca9fe3e7363f5fbf0a2c3ab
Nicolai Emig <nicolai.emig@amseegym.de>
2021-02-15 09:38:02 +0100

    Planetenlösung wieder hinzugefügt

---
+++ Lektionen/03.Planetensystem/src/commonMain/kotlin/Gesteinsplanet.kt
@@ -0,0 +1,15 @@
+import com.soywiz.korim.color.RGBA
+
+class Gesteinsplanet(
+        groesse : Double,
+        bahnradius : Double,
+        farbe : RGBA,
+        drehtSichUm : Stern,
+        mitGeschwindigkeit : Double
+) : Planet(
+        planetenRadius = groesse,
+        bahnradius = bahnradius,
+        farbe = farbe,
+        drehtSichUm = drehtSichUm,
+        mitGeschwindigkeit = mitGeschwindigkeit
+)
