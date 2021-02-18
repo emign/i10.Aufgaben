968ee74f0a7364bc9c2c1c3068d0aa34887692e0
Nicolai Emig <nicolai.emig@amseegym.de>
2021-02-18 08:32:25 +0100

    Staubsauger eingeführt

--- Lektionen/05.Staubsauger/build.gradle.kts
+++ Lektionen/05.Staubsauger/build.gradle.kts
@@ -3,7 +3,7 @@ import com.soywiz.korge.gradle.*
 apply<KorgeGradlePlugin>()
 
 korge {
-    id = "me.emig.lektion.04-Sortieralgorithmen"
+    id = "me.emig.lektion.05-Staubsauger"
     targetJvm()
     targetJs()
 }
---
+++ Lektionen/05.Staubsauger/src/commonMain/kotlin/main.kt
@@ -0,0 +1,10 @@
+import com.soywiz.klock.milliseconds
+import com.soywiz.korge.Korge
+import com.soywiz.korge.view.addUpdater
+import com.soywiz.korim.color.Colors
+
+suspend fun main() = Korge(width = 800, height = 600, bgcolor = Colors["#2b2b2b"], title = "Staubsauger") {
+
+
+}
+
