d616699ba1b59fd30c80b3faed62662433a3eff8
Nicolai Emig <nicolai.emig@amseegym.de>
2021-02-25 08:46:34 +0100

    Bogosort umgedreht

--- Lektionen/04.Sortieralgorithmen/src/commonMain/kotlin/BogoSort.kt
+++ Lektionen/04.Sortieralgorithmen/src/commonMain/kotlin/BogoSort.kt
@@ -4,20 +4,21 @@ object BogoSort : Sortieralgorithmus(){
     override var name: String = "BogoSort"
 
     override suspend fun sortieren(array: Array<SolidRect>) {
-        while (!isSorted(array)){
+
+        while (unsortiert(array)){
             array.shuffle()
-            SortierRoboter.countVergleich()
             SortierRoboter.positionenAktualisieren()
+            SortierRoboter.countVergleich()
         }
     }
 
-    private fun isSorted(array: Array<SolidRect>) : Boolean {
+    private fun unsortiert(array: Array<SolidRect>) : Boolean {
         var sortiert = true
         for (i in 0 until array.indices.last){
             if (array[i].height > array[i+1].height){
                 sortiert = false
             }
         }
-        return sortiert
+        return !sortiert
     }
 }
